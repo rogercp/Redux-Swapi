1b4180e4cd0ae1c9dd739800d10fc4e467cbfb71
Roger Caballero <rogercp1234@yahoo.com>
2019-05-15 13:30:55 -0700

    made axios call and sent to reducers

--- src/reducers/starWarsReducer.js
+++ src/reducers/starWarsReducer.js
@@ -1,6 +1,8 @@
-import /* we need our action types here*/ "../actions";
+import /* we need our action types here*/  {FETCHING ,  SUCCESS  ,   FAILURE }       from "../actions";
 const initialState = {
-  characters: []
+  characters: [],
+  fetching:false,
+  error:"",
   // Array characters, Boolean fetching, null error.
 };
 export const charsReducer = (state = initialState, action) => {
@@ -8,6 +10,31 @@ export const charsReducer = (state = initialState, action) => {
     // Fill me in with the important reducers
     // action types should be FETCHING, SUCCESS and FAILURE
     // your switch statement should handle all of these cases.
+
+    case FETCHING:
+    return {
+      ...state,
+      fetching:true,
+      error:''
+    }
+    
+    case SUCCESS:
+    return {
+      ...state,
+      fetching:false,
+      characters:action.payload,
+      error:''
+    }
+    
+
+    case FAILURE:
+    return {
+      ...state,
+      fetching:false,
+      error:action.payload
+
+    }
+
     default:
       return state;
   }
--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,6 +3,7 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import {getCharacters} from "../actions"
 
 class CharacterListView extends React.Component {
   constructor() {
@@ -10,7 +11,7 @@ class CharacterListView extends React.Component {
   }
 
   componentDidMount() {
-    // call our action
+    this.props.getCharacters();
   }
 
   render() {
@@ -27,9 +28,20 @@ class CharacterListView extends React.Component {
 
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
+
+const mapStateToProps=state=>({
+  characters:state.characters,
+  fetching:state.fetching,
+  error:state.error
+  
+})
+
+
+
+
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps,
   {
-    /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
